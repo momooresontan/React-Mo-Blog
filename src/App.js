4f923a2e426274a5cf655ba9d589e06804a93f8a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-09 16:44:06 +0100

    post & header components created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import Post from "./Post";
 import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route
+        index
+        element={
+          <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+        }
+      />
+      <Route path={"/login"} element={<div>Login</div>} />
+      <Route path={"/register"} element={<div>Register</div>} />
+    </Routes>
   );
 }
 

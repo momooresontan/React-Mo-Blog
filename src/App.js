db409f02ea14775f7423dcbf7600dc3666f23d51
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-09 14:55:39 +0100

    css modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MoBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/08/nvidia-siggraph1.jpg?w=850&h=492&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Nvidia CEO: We bet the farm on AI and no one knew it</h2>
+          <p className="info">
+            <a className="author">Momooreoluwa Sontan</a>
+            <time>2023-08-09 14:38</time>
+          </p>
+          <p className="summary">
+            Nvidia founder and CEO Jensen Huang said today that the company made
+            an existential business decision in 2018 that few realized would
+            redefine its future and help redefine an evolving industry.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/08/nvidia-siggraph1.jpg?w=850&h=492&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Nvidia CEO: We bet the farm on AI and no one knew it</h2>
+          <p className="info">
+            <a className="author">Momooreoluwa Sontan</a>
+            <time>2023-08-09 14:38</time>
+          </p>
+          <p className="summary">
+            Nvidia founder and CEO Jensen Huang said today that the company made
+            an existential business decision in 2018 that few realized would
+            redefine its future and help redefine an evolving industry.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/08/nvidia-siggraph1.jpg?w=850&h=492&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="text">
+          <h2>Nvidia CEO: We bet the farm on AI and no one knew it</h2>
+          <p className="info">
+            <a className="author">Momooreoluwa Sontan</a>
+            <time>2023-08-09 14:38</time>
+          </p>
+          <p className="summary">
+            Nvidia founder and CEO Jensen Huang said today that the company made
+            an existential business decision in 2018 that few realized would
+            redefine its future and help redefine an evolving industry.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 

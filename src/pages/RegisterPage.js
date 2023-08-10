f319ffd87ecb7e40141f5ef0f90308736a4e7dea
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-10 08:44:52 +0100

    resgister & login component & css modified

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
 export default function RegisterPage() {
   return (
     <form className="register">
+      <h1>Register</h1>
       <input type="text" placeholder="username" />
       <input type="email" name="" id="" placeholder="email" />
       <input type="password" placeholder="password" />

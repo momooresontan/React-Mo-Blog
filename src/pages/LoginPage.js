01d3d84871f9ea3476a72d13f4a4e8640eabb077
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-11 08:45:32 +0100

    login & register components modified

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login(e) {
+    e.preventDefault();
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({username, password}),
+      headers: { 'Content-Type':  }
+    });
+  }
+
   return (
-    <form className="login">
+    <form className="login" onSubmit={login}>
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
     </form>
   );

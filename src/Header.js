0af404ea053d169274a51d9e0e9da0a6384c21d4
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-13 04:55:19 +0100

    header component modified

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  useEffect(() => {
+    fetch("http://localhost:4000/getMe", {
+      credentials: "include",
+    });
+  }, []);
   return (
     <header>
       <a href="/" className="logo">

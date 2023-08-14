445a007082c0307debe3930cf15b295dbcaffc53
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-14 01:00:56 +0100

    user context created

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 
 export const UserContext = createContext({});
+
+export default function UserContextProvider({ children }) {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
+}

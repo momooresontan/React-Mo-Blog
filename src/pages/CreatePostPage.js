b8da099c9495f44c2b6edf31418201372c58b3b4
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-16 00:29:41 +0100

    create new post component modified

--- src/pages/CreatePostPage.js
+++ src/pages/CreatePostPage.js
@@ -1,13 +1,62 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
 export default function CreatePostPage() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <form>
-      <input type="title" placeholder={"Title"} />
-      <input type="summary" placeholder={"Summary"} />
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
       <input type="file" />
-      <ReactQuill />
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+        formats={formats}
+      />
+      <button style={{ marginTop: "5px" }}>Create Post</button>
     </form>
   );
 }

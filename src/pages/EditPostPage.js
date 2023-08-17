368a937b504e1da53e79d4083564de002b3ce0f4
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-17 18:12:59 +0100

    edit post component modified

--- src/pages/EditPostPage.js
+++ src/pages/EditPostPage.js
@@ -1,3 +1,67 @@
+import { formatISO } from "date-fns";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
 export default function EditPostPage() {
-  return <div>Edit post page</div>;
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function editPost(e) {}
+
+  return (
+    <form onSubmit={editPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+        formats={formats}
+      />
+      <button style={{ marginTop: "5px" }}>Edit Post</button>
+    </form>
+  );
 }
